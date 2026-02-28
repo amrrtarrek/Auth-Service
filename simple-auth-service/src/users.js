1a2b250a8035e739292720f3daf733529077f2ce
amr tarek <amrtarekadel14@gmail.com>
2026-02-28 22:19:22 +0200

    Task 4
    Move the hardcoded users from src/users.js into a separate data/users.json file and update the import logic.

--- simple-auth-service/src/users.js
+++ simple-auth-service/src/users.js
@@ -1,10 +1,9 @@
-const users = [
-  { id: 1, username: "admin", password: "admin123", role: "admin" },
-  { id: 2, username: "user", password: "user123", role: "user" }
-];
+
+const users = require("../data/users.json");
 
 function findUser(username) {
   return users.find(u => u.username === username);
 }
 
 module.exports = { findUser };
+
